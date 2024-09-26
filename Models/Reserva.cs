299626e9008d988d5da3379073c75639a98a2a5c
andersonrribas <ander.ribas@gmail.com>
2024-09-26 17:02:11 -0300

    Descrição: Desafio Dio: Construindo um Sistema de Hospedagem de um Hotel no C#

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata;
+
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -15,16 +17,16 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido           
+            if (hospedes.Count() <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
+                Console.WriteLine("Hóspedes cadastrados com sucesso.");
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new InvalidOperationException("Não é possível cadastrar os hóspedes: o número excede a capacidade da suíte.");
             }
         }
 
@@ -36,22 +38,21 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count();
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            const decimal desconto = 0.1m;
+
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor *= (1 - desconto);
             }
 
             return valor;
